2b7ae59e18aa8dbb46a74fa92849ec9fe75b7978
arfirmaniak3217 <arfirman.iak3217@gmail.com>
2017-08-14 18:36:31 +0700

    Create DetailActivity and connected intent from MainActivity

---
+++ app/src/main/java/io/arfirman1402/dev/mybeautifulsunshine/DetailActivity.java
@@ -0,0 +1,19 @@
+package io.arfirman1402.dev.mybeautifulsunshine;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by alodokter-it on 14/08/17 -- DetailActivity.
+ */
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+    }
+
+}
--- app/src/main/java/io/arfirman1402/dev/mybeautifulsunshine/WeatherAdapter.java
+++ app/src/main/java/io/arfirman1402/dev/mybeautifulsunshine/WeatherAdapter.java
@@ -1,9 +1,17 @@
 package io.arfirman1402.dev.mybeautifulsunshine;
 
+import android.content.Context;
+import android.content.Intent;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by alodokter-it on 14/08/17 -- WeatherAdapter.
@@ -27,10 +35,38 @@ public class WeatherAdapter extends RecyclerView.Adapter<WeatherAdapter.WeatherV
         return 3;
     }
 
-    public class WeatherViewHolder extends RecyclerView.ViewHolder {
+    public class WeatherViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        @BindView(R.id.cv_adapter_weather)
+        CardView adapterWeather;
+
+        @BindView(R.id.img_adapter_weather)
+        ImageView adapterWeatherImage;
+
+        @BindView(R.id.txt_adapter_weather_date)
+        TextView adapterWeatherDate;
+
+        @BindView(R.id.txt_adapter_weather_desc)
+        TextView adapterWeatherDesc;
+
+        @BindView(R.id.txt_adapter_weather_temperature)
+        TextView adapterWeatherTemperature;
 
         public WeatherViewHolder(View itemView) {
             super(itemView);
+
+            ButterKnife.bind(this, itemView);
+
+            adapterWeather.setOnClickListener(this);
         }
+
+        @Override
+        public void onClick(View v) {
+            showDetail(v.getContext());
+        }
+    }
+
+    private void showDetail(Context context) {
+        Intent intent = new Intent(context, DetailActivity.class);
+        context.startActivity(intent);
     }
 }
